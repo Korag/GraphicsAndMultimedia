ae4acecc9d64845c086b6712773fdcd3b6b16cf7
Korag <koragrex@gmail.com>
2020-03-07 10:10:35 +0100

    Added 2D Platformer project (currently with errors due to newer Unity version than was presented in laboratory instruction)

---
+++ GIM_Czepielik_Łukasz_2. 2D Platformer/2D Platformer/Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void OnCollision2D(GameObject other)
+    {
+        // Delete our Platforms as they fall
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            Destroy(other);
+        }
+    }
+}
---
+++ GIM_Czepielik_Łukasz_2. 2D Platformer/2D Platformer/Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCoin : MonoBehaviour
+{
+    // For now just destroy the coin if the player runs into
+    // it. Could play a sound, add to a score or more here later
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Coin was picked up");
+        }
+    }
+}
