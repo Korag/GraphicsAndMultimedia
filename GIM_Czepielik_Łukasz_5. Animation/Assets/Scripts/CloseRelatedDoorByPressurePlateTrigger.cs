2169bbf13b9ffd9bd973f100e9e814783bace4d9
Łukasz Czepielik <koragrex@gmail.com>
2020-03-27 01:14:10 +0100

    Updated Animation project

---
+++ GIM_Czepielik_Łukasz_5. Animation/Assets/Scripts/CloseRelatedDoorByPressurePlateTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseRelatedDoorByPressurePlateTrigger : MonoBehaviour
+{
+    public GameObject doorPanel;
+
+    [HideInInspector]
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator = doorPanel.GetComponent<Animator>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ThirdPersonControllerPlayer") && animator.GetBool("DoorOpened"))
+        {
+            animator.SetBool("DoorClosed", true);
+            animator.SetBool("DoorOpened", false);
+
+            this.gameObject.SetActive(false);
+
+            ScoreController scoreController = other.gameObject.GetComponent<ScoreController>();
+            scoreController.SetScore();
+        }
+    }
+}
