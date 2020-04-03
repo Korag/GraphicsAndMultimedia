da5d1d7bdb7701a9d97ac21f204020775f03034b
Łukasz Czepielik <koragrex@gmail.com>
2020-04-03 02:05:32 +0200

    Added particles generators for rain and snow weather to 7 project.

---
+++ GIM_Czepielik_Łukasz_7. Particle System/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    public GameObject snowObject;
+    public GameObject rainObject;
+
+    private ParticleSystem _snowParticles;
+    private ParticleSystem _rainParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _snowParticles = snowObject.GetComponent<ParticleSystem>();
+        _rainParticles = rainObject.GetComponent<ParticleSystem>();
+        
+        _snowParticles.Stop();
+        _rainParticles.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            _snowParticles.Stop();
+            _rainParticles.Stop();
+        }
+
+        else if (Input.GetKey(KeyCode.K))
+        {
+            _snowParticles.Play();
+            _rainParticles.Stop();
+        }
+
+        else if (Input.GetKey(KeyCode.L))
+        {
+            _snowParticles.Stop();
+            _rainParticles.Play();
+        }
+    }
+}
